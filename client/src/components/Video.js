33976a1444209fbf6658b09f3125311a58453398
Micaela Colpi <micaelacolpi@gmail.com>
2021-07-12 15:17:43 -0500

    finished chapter 7 setup

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card, CardBody } from "reactstrap";
-
+import { Link } from "react-router-dom";
 
 const Video = ({ video }) => {
     return (
@@ -15,7 +15,9 @@ const Video = ({ video }) => {
                     allowFullScreen />
 
                 <p>
-                    <strong>{video.title}</strong>
+                    <Link to={`/videos/${video.id}`}>
+                        <strong>{video.title}</strong>
+                    </Link>
                 </p>
                 <p>{video.description}</p>
                 {video.comments?.map(comment => (<p key={comment.id}> {comment.message}</p>))}
