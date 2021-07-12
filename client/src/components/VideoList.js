45e6515ad6c031b7974d4c6cafc05b90bb868108
Micaela Colpi <micaelacolpi@gmail.com>
2021-07-12 13:50:00 -0500

    finished chapter 6 exercise 2

--- client/src/components/VideoList.js
+++ client/src/components/VideoList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Video from './Video';
-import { getAllVideos, getAllVideosWithComments } from "../modules/videoManager";
+import { getAllVideos, getAllVideosWithComments, getVideosFromSearch } from "../modules/videoManager";
 
 const VideoList = () => {
     const [videos, setVideos] = useState([]);
@@ -13,18 +13,30 @@ const VideoList = () => {
         getAllVideosWithComments().then(videos => setVideos(videos));
     };
 
+    const handleinput = (e) => {
+        let enteredValue = e.target.value;
+        getVideosFromSearch(enteredValue).then(videos => setVideos(videos));
+    }
+
     useEffect(() => {
         getVideosWithComments();
     }, []);
 
+
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                {videos.map((video) => (
-                    <Video video={video} key={video.id} />
-                ))}
+        <>
+            <div>
+                <input type="text" placeholder="search videos" onChange={handleinput} ></input>
             </div>
-        </div>
+
+            <div className="container">
+                <div className="row justify-content-center">
+                    {videos.map((video) => (
+                        <Video video={video} key={video.id} />
+                    ))}
+                </div>
+            </div>
+        </>
     );
 };
 
